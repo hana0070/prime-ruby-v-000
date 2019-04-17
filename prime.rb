7f77d3b8165a46158f705d1692c3b914d5669a78
HANA LEANDER <hana0070@gmail.com>
2019-04-17 13:59:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,6 +6,17 @@ def prime?(num)
     (2..num-1).to_a.all? do |possible_factor|
         num % possible_factor != 0
       end
-    return true
   end
 end
+
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
+
+puts prime?(105557)
